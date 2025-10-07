acadf27ff40c59155496d7d98edde5993a93ec35
LKenji <54600289+lucaskmk@users.noreply.github.com>
2025-10-07 15:19:13 -0300

    FOI rapido 2

--- main/main.c
+++ main/main.c
@@ -1,6 +1,5 @@
 #include "FreeRTOS.h"
 #include "task.h"
-#include "semphr.h"
 #include "queue.h"
 #include <stdio.h>
 #include <math.h>
@@ -14,16 +13,21 @@
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 #define EOP_BYTE 0xFF
-#define NUM_SAMPLES 32
-#define READ_MS 120
-#define DEAD_ZONE 300
-#define TARGET_DELTA 10
-#define ZERO_HOLD_CNT 4
-#define RAMP_STEP 2
-#define SEND_INTERVAL_MS 80
 
-static volatile int16_t target_vals[2] = {0,0};
-static volatile int16_t current_vals[2] = {0,0};
+#define NUM_SAMPLES        32
+#define READ_MS            120
+#define DEAD_ZONE          300
+#define TARGET_DELTA       10
+#define ZERO_HOLD_CNT      4
+#define RAMP_STEP          2
+#define SEND_INTERVAL_MS   80
+
+typedef struct {
+    uint8_t axis;
+    int16_t target;
+} target_msg_t;
+
+static QueueHandle_t xQueueTargets = NULL;
 
 static inline int map_int(int x, int in_min, int in_max, int out_min, int out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -41,22 +45,16 @@ void uart_init_all(void) {
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 }
 
-static void set_target_safe(int axis, int new_target) {
-    taskENTER_CRITICAL();
-    int16_t prev = target_vals[axis];
-    if (abs(new_target - prev) >= TARGET_DELTA) target_vals[axis] = (int16_t)new_target;
-    taskEXIT_CRITICAL();
-}
-
 void axis_reader_loop(int adc_channel, int axis_id) {
     uint16_t samples[NUM_SAMPLES];
     int idx = 0;
     int zero_hold = 0;
+    int16_t last_local_target = 0;
     const TickType_t period = pdMS_TO_TICKS(READ_MS);
     for (int i = 0; i < NUM_SAMPLES; ++i) {
         adc_select_input(adc_channel);
         samples[i] = adc_read();
-        sleep_ms(1);
+        vTaskDelay(pdMS_TO_TICKS(1));
     }
     for (;;) {
         adc_select_input(adc_channel);
@@ -70,15 +68,17 @@ void axis_reader_loop(int adc_channel, int axis_id) {
         if (mapped > -DEAD_ZONE && mapped < DEAD_ZONE) {
             zero_hold++;
             if (zero_hold >= ZERO_HOLD_CNT) mapped = 0;
-            else {
-                taskENTER_CRITICAL();
-                mapped = target_vals[axis_id];
-                taskEXIT_CRITICAL();
-            }
+            else mapped = last_local_target;
         } else {
             zero_hold = 0;
         }
-        set_target_safe(axis_id, mapped);
+        if (abs(mapped - last_local_target) >= TARGET_DELTA) {
+            target_msg_t m;
+            m.axis = (uint8_t)axis_id;
+            m.target = (int16_t)mapped;
+            xQueueSend(xQueueTargets, &m, 0);
+            last_local_target = m.target;
+        }
         vTaskDelay(period);
     }
 }
@@ -86,44 +86,43 @@ void axis_reader_loop(int adc_channel, int axis_id) {
 void x_task(void *pv) { (void)pv; axis_reader_loop(ADC_X_CHANNEL, 0); }
 void y_task(void *pv) { (void)pv; axis_reader_loop(ADC_Y_CHANNEL, 1); }
 
-static void send_packet_lsb_first(uint8_t axis, int16_t value) {
+static void send_packet_msb_first(uint8_t axis, int16_t value) {
     uint8_t msb = (uint8_t)((value >> 8) & 0xFF);
     uint8_t lsb = (uint8_t)(value & 0xFF);
-    uart_putc_raw(UART_ID, EOP_BYTE);
     uart_putc_raw(UART_ID, axis);
-    uart_putc_raw(UART_ID, lsb);
     uart_putc_raw(UART_ID, msb);
+    uart_putc_raw(UART_ID, lsb);
+    uart_putc_raw(UART_ID, EOP_BYTE);
 }
 
 void uart_ramp_task(void *pvParameters) {
     (void)pvParameters;
+    int16_t target_vals[2] = {0,0};
+    int16_t current_vals[2] = {0,0};
+    int16_t last_sent[2] = {32767,32767};
     const TickType_t period = pdMS_TO_TICKS(SEND_INTERVAL_MS);
-    int16_t last_sent[2] = {0, 0};
+    target_msg_t m;
     for (;;) {
+        while (xQueueReceive(xQueueTargets, &m, 0) == pdPASS) {
+            if (m.axis < 2) target_vals[m.axis] = m.target;
+        }
         for (int axis = 0; axis < 2; ++axis) {
-            int16_t tgt, cur;
-            taskENTER_CRITICAL();
-            tgt = target_vals[axis];
-            cur = current_vals[axis];
-            taskEXIT_CRITICAL();
+            int16_t tgt = target_vals[axis];
+            int16_t cur = current_vals[axis];
             if (tgt != cur) {
                 int delta = tgt - cur;
                 int step = delta;
                 if (abs(delta) > RAMP_STEP) step = (delta > 0) ? RAMP_STEP : -RAMP_STEP;
                 cur = cur + step;
-                taskENTER_CRITICAL();
                 current_vals[axis] = cur;
-                taskEXIT_CRITICAL();
                 if (cur != last_sent[axis]) {
-                    send_packet_lsb_first((uint8_t)axis, cur);
+                    send_packet_msb_first((uint8_t)axis, cur);
                     last_sent[axis] = cur;
                 }
-                sleep_ms(2);
             } else {
                 if (cur != last_sent[axis]) {
-                    send_packet_lsb_first((uint8_t)axis, cur);
+                    send_packet_msb_first((uint8_t)axis, cur);
                     last_sent[axis] = cur;
-                    sleep_ms(2);
                 }
             }
         }
@@ -135,8 +134,8 @@ int main(void) {
     stdio_init_all();
     adc_init_all();
     uart_init_all();
-    target_vals[0] = target_vals[1] = 0;
-    current_vals[0] = current_vals[1] = 0;
+    xQueueTargets = xQueueCreate(8, sizeof(target_msg_t));
+    if (xQueueTargets == NULL) for (;;);
     xTaskCreate(x_task, "x_task", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
     xTaskCreate(y_task, "y_task", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
     xTaskCreate(uart_ramp_task, "uart_ramp", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
